8089f9547d7977e61b312597a340c44a00d77f09
Williancosta98 <willian.c0sta4167@gmail.com>
2025-05-29 13:24:11 -0300

    subindo o projeto JS

---
+++ script.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("#main-input")
+
+const paragraph = document.querySelector(".paragraph-show")
+
+
+function cliqueiNoBotao(){
+    paragraph.innerHTML = input.value
+}
+
+function digiteiNoInput(){
+    console.log("Digitei no Input")
+}
